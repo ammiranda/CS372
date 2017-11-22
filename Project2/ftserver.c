88f9d0b34f4960eaacc2e9d4d764806372d61e8b
ammiranda <alexandermichaelmiranda@gmail.com>
2017-11-22 14:27:58 -0800

    Initial commit for Project2 work

---
+++ Project2/ftserver.c
@@ -0,0 +1,9 @@
+/**
+ * Name: Alexander Miranda
+ * Assignment: Project 2 File Transfer client/server
+ * Due Date: 11/26/2017
+ * 
+ * References: http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
+*/
+
+#include <stdio.h>
